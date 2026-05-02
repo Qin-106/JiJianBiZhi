9c9e3ec0193c3d811b7a424c5400834856ea1bd5
Qin-106 <2914982547@QQ.COM>
2026-05-03 02:10:40 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,33 @@
-const CACHE_NAME = "wallpaper-v1";
-const urlsToCache = ["./", "index.html", "icon.png"];
+const CACHE_NAME = "wallpaper-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/service-worker.js",
+  "/icon.png"
+];
 
-self.addEventListener("install", e =>
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)))
-);
+// 安装时缓存资源
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
 
-self.addEventListener("fetch", e =>
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
-);
+// 拦截请求，优先用缓存
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
+
+// 激活时清理旧缓存
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+});
